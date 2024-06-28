43cd1c4e938ba46fa75ff40bb0f1fb93408dd044
moth <dinocruz434@gmail.com>
2024-06-28 16:37:56 +0100

    Grouping routes and views

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,29 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/posts/create', function () {
-    return view('create');
-})->name('posts.create');
+Route::name('posts.')->prefix('posts')->group(function () {
+    Route::get('/create', function () {
+        return view('posts.create');
+    })->name('create');
 
-
-Route::post('/posts', function (Request $request) {
-    $validated = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-    ]);
+    Route::post('/posts', function (Request $request) {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+        ]);
+        
+        $title = $validated['title'];
+        $description = $validated['description'];
     
-    $title = $validated['title'];
-    $description = $validated['description'];
+        return redirect()
+            ->route('posts.create')
+            ->with(
+                'success' , "Post is submitted! Title: {$title} Description: {$description}"
+            );
+    })->name('store');
+});
 
-    return redirect()
-        ->route('posts.create')
-        ->with(
-            'success' , "Post is submitted! Title: {$title} Description: {$description}"
-        );
-})->name('posts.store');
+
+
+
+
