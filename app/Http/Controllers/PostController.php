9897e81db78e2dfaff8c6ec4aeeea907735c87c3
moth <dinocruz434@gmail.com>
2024-07-09 12:29:48 +0100

    Spoofing form actions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -56,7 +56,7 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return view('posts.edit', ['post' => Post::findOrFail($id)]);
     }
 
     /**
