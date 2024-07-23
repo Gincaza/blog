5b1cd49cd5beee152974a20b83e324fe9005cec8
moth <dinocruz434@gmail.com>
2024-07-23 12:07:20 +0100

    Authorization in blades

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,12 +7,19 @@
     <div class='post-content'>
         <h2>{{ $post->title }}</h2>
         <p>{{ $post->description }}</p>
+        
+        @can('update', $post)
         <a href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+        @endcan
+
+        @can('delete', $post)
         <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="post">
             @csrf
             @method('DELETE')
             <button class="delete-button" type="submit">Delete</button>
         </form>
+        @endcan
+        
         <small>Posted by <b>{{ $post->user->name }}</b></small>
     </div>
 </div>
