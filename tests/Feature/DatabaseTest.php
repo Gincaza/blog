04d0179088b317e1ae34c18b83c3e873540e7081
mothnue <dinocruz434@gmail.com>
2024-07-29 11:00:53 +0100

    Testing models and the Relationships

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -6,10 +6,11 @@ use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class DatabaseTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use RefreshDatabase;
     public function test_post_model(): void
     {
         $post = Post::factory()->create();
@@ -18,5 +19,13 @@ class DatabaseTest extends TestCase
             'title' => $post->title,
             'description' => $post->description,
         ]);
+    }
+
+    public function test_posts_user_relationship(): void
+    {
+        $user = User::factory()->has(Post::factory()->count(3))->create();
+        $this->assertDatabaseCount('posts', 3);
+        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseHas('posts', [ 'user_id' => $user->id ]);
     }
 }
